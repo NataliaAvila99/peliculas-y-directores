40324dc2b9bddf0ea485c99d8eea8a9950655821
NataliaAvila99 <natalia.ignacia@hotmail.com>
2024-11-05 22:19:17 -0300

    peliculas y directores

---
+++ src/main/java/com/tunombre/PeliculasyDirectoresApplication.java
@@ -0,0 +1,13 @@
+package com.tunombre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasyDirectoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasyDirectoresApplication.class, args);
+	}
+
+}
